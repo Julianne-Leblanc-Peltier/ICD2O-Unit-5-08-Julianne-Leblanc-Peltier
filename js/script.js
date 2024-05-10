00d3b903f8ec6b53074b1768c3d5249c4cfc97e3
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-10 18:19:16 +0000

    js vers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const firstNumber = parseInt(document.getElementById("first-number").value)
+  const secondNumber = parseInt(document.getElementById("second-number").value)
+  let remainder = firstNumber
+  let counter = 0
+
+  while (true) {
+    remainder -= secondNumber
+    counter ++
+    if (remainder < secondNumber) {
+      break
+    }
+  }
+
+  document.getElementById("answer").innerHTML = counter + " R" + remainder
+}
